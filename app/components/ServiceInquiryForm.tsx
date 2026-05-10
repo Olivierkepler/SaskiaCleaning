3ee31bf889c61132d888ecfd80697e9c53595a3e
Olivier Kepler Francois <98571148+Olivierkepler@users.noreply.github.com>
2026-05-10 18:49:43 -0400

    Add z-index to ServiceInquiryForm styles

--- app/components/ServiceInquiryForm.tsx
+++ app/components/ServiceInquiryForm.tsx
@@ -421,6 +421,7 @@ export default function ServiceInquiryForm({ onClose }: ServiceInquiryFormProps)
       display: "flex",
       alignItems: "flex-start",
       justifyContent: "center",
+        z-index: 100;
     }}>
       <style>{`
         @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;1,300;1,400&family=DM+Sans:wght@300;400;500&display=swap');
