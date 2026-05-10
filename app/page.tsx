1422cdd86a17cacc8dbced2c53eea3f8f3b818c1
Olivier Kepler Francois <98571148+Olivierkepler@users.noreply.github.com>
2026-05-10 19:52:21 -0400

    Add InfoBar component to the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import CleaningPricingCalculator from './components/CleaningPricingCalculator';
 import { motion } from 'framer-motion';
 import AutoServiceCarousel from './components/AutoServiceCarousel';
 import CleaningPriceCalculator from './components/CleaningPriceCalculator';
+import InfoBar from "./components/InfoBar";
 
 export default function Home() {
   
@@ -17,7 +18,7 @@ export default function Home() {
       
       {/* 1. Hero: Internal entrance animations already set */}
       <Hero />
-
+<InfoBar />
       {/* 2. TrustBar: Gentle slide-in reveal */}
       {/* <SectionWrapper>
         <TrustBar />
