83f52b117873b1098528a508cda1856fd5a9d1f0
Olivier Kepler Francois <98571148+Olivierkepler@users.noreply.github.com>
2026-05-12 22:30:00 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from './components/Navbar';
+
+  {/* import Navbar from './components/Navbar'; */}
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
@@ -30,7 +31,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-      <Navbar />
+    
+          {/*   <Navbar /> */}
         {children}
         <Footer/>
         
